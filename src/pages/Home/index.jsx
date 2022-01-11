a2d6d17f205c0e051ad43a0769b37b3118b3b1eb
wellison E Brito <wellysone.brito@gmail.com>
2022-01-11 13:44:13 -0300

    Home Page Created

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../components/Header";
+import "./styles.scss";
+
+export const Home = () => {
+    return (
+        <main className='container'>
+            <Header />
+        </main>
+    );
+};
