d0125df50653d36f567004132e9ced39fd400142
wellison E Brito <wellysone.brito@gmail.com>
2022-01-11 14:15:34 -0300

    Page Home Changed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
 import "./styles.scss";
 
 export const Home = () => {
     return (
         <main className='container'>
             <Header />
+            <SideBar />
         </main>
     );
 };
