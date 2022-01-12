277d99bed555ea7aae2612ffe1ce43b5498f86be
wellison E Brito <wellysone.brito@gmail.com>
2022-01-12 17:21:40 -0300

    Header Component Finished

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,26 @@
 import React from "react";
+import MarvelLogoImg from "../../assets/MarvelLogo.svg";
+import SpiderManLogoImg from "../../assets/SpiderManLogo.svg";
 import "./styles.scss";
 
 export const Header = () => {
     return (
-        <div className='header-container'>
-            <nav className='nav-header'>
-                <ul className='nav-header-text'>
-                    <li>
-                        <img src='' alt='' />
-                    </li>
-                    <li>
-                        <a href=''></a>
-                    </li>
-                    <li>
-                        <div>
-                            <label>home</label>
-                            <label>sinopse</label>
-                            <label>elenco</label>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <nav className='header-container'>
+            <ul className='nav-header-text'>
+                <li>
+                    <img src={MarvelLogoImg} alt='logo-marvel' />
+                </li>
+                <li>
+                    <img src={SpiderManLogoImg} alt='logo-spider-man' />
+                </li>
+                <li>
+                    <div className='nav-menu-text'>
+                        <label>home</label>
+                        <label>sinopse</label>
+                        <label>elenco</label>
+                    </div>
+                </li>
+            </ul>
+        </nav>
     );
 };
