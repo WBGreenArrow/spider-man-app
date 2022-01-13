e1e123368a5646a13f5b001af0eb593bfb697b1c
wellison E Brito <wellysone.brito@gmail.com>
2022-01-13 15:03:59 -0300

    Changed Styles Main Components

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./styles.scss";
 
+import SpiderManMilesMorales from "../../assets/SpiderMan-Miles-Morales.svg";
+
 export const Main = () => {
-    return <div className='main-container'></div>;
+    return (
+        <div className='main-container'>
+            <div className='main-content'>
+                <img src={SpiderManMilesMorales} alt='Spider Man Miles Morales' />
+                <div>
+                    assista ao trailer
+                    <label></label>
+                </div>
+            </div>
+        </div>
+    );
 };
