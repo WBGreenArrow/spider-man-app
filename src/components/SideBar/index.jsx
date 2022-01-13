ded5242ca74bafb6df2ba5b2bea9e4367ba957e0
wellison E Brito <wellysone.brito@gmail.com>
2022-01-13 13:29:16 -0300

    SideBar Component Finished

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./styles.scss";
 
+import InstagramLogo from "../../assets/instagram.svg";
+import TwitterLogo from "../../assets/twitter.svg";
+import FacebookLogo from "../../assets/facebook.svg";
+import SearchLogo from "../../assets/search.svg";
+
 export const SideBar = () => {
     return (
         <div className='side-bar-conatiner'>
             <aside className='side-bar-content'>
                 <img src='https://github.com/WBGreenArrow.png' alt='profile-photo' />
                 <div className='online'></div>
-                <div>
-                    <label>Instagram</label>
-                    <label>Twitter</label>
-                    <label>Facebook</label>
+                <div className='side-bar-network-content'>
+                    <img src={InstagramLogo} alt='instagrm-logo' />
+                    <img src={TwitterLogo} alt='twitter-logo' />
+                    <img src={FacebookLogo} alt='facebook-logo' />
                 </div>
-                <label>Search</label>
+                <img className='side-bar-search-logo' src={SearchLogo} alt='search-logo' />
             </aside>
         </div>
     );
